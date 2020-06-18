ae23e8429bf007e62d762d73b47684d6b28d64d8
Rikin <rmarfatia@pinterest.com>
2020-06-18 08:19:51 -0700

    [Daily Tracking] Added a goal display, done state, and profile pic

--- app/src/main/java/me/rikinmarfatia/hydrohomie/MainActivity.kt
+++ app/src/main/java/me/rikinmarfatia/hydrohomie/MainActivity.kt
@@ -7,24 +7,34 @@ import androidx.compose.getValue
 import androidx.compose.setValue
 import androidx.compose.state
 import androidx.ui.core.Alignment
+import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
 import androidx.ui.core.clip
+import androidx.ui.core.drawShadow
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Box
 import androidx.ui.foundation.ContentGravity
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.drawBorder
+import androidx.ui.foundation.shape.corner.CircleShape
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
+import androidx.ui.layout.Arrangement
 import androidx.ui.layout.Column
 import androidx.ui.layout.Row
+import androidx.ui.layout.Spacer
 import androidx.ui.layout.Stack
 import androidx.ui.layout.fillMaxSize
 import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.height
 import androidx.ui.layout.padding
+import androidx.ui.layout.size
 import androidx.ui.layout.width
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
+import androidx.ui.text.style.TextAlign
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.Dp
 import androidx.ui.unit.dp
@@ -64,12 +74,14 @@ fun WaterContainer() {
             modifier = Modifier.fillMaxWidth(),
             horizontalGravity = Alignment.CenterHorizontally
         ) {
+            ProfilePic()
+            Spacer(modifier = Modifier.height(16.dp))
             WaterBackground(waterState)
             Row {
                 Button(
                     onClick = addWaterAction,
                     modifier = Modifier
-                        .padding(16.dp)
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
                         .clip(MaterialTheme.shapes.large),
                     backgroundColor = hydroBlue
                 ) {
@@ -78,13 +90,14 @@ fun WaterContainer() {
                 Button(
                     onClick = { waterState = waterState.copy(count = 0) },
                     modifier = Modifier
-                        .padding(16.dp)
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
                         .clip(MaterialTheme.shapes.large),
                     backgroundColor = Color.Red
                 ) {
                     Text(text = "Reset", color = MaterialTheme.colors.onSurface)
                 }
             }
+            GoalDisplay(waterState = waterState)
         }
     }
 }
@@ -119,7 +132,47 @@ fun WaterBackground(state: WaterState) {
             backgroundColor = hydroBlue
         )
     }
+}
 
+@Composable
+fun GoalDisplay(waterState: WaterState) {
+
+    fun display(state: WaterState): String {
+        val cupsLeft = waterState.goal - waterState.count
+        return when {
+            cupsLeft > 0 -> "${waterState.count} / ${waterState.goal}"
+            else -> "Done"
+        }
+    }
+
+    Text(
+        text = display(waterState),
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.h4,
+        color = MaterialTheme.colors.onSurface
+    )
+}
+
+@Composable
+fun ProfilePic() {
+    val image = imageResource(id = R.drawable.dummy_image)
+    val imageModifier = Modifier
+        .size(60.dp)
+        .drawShadow(8.dp, CircleShape)
+        .drawBorder(4.dp, hydroBlue, CircleShape)
+
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            asset = image,
+            modifier = imageModifier,
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
