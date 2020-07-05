325476b37e63d21cca00e409a0a0fc8fdd088d5c
rmarfatia <rmarfatia@pinterest.com>
2020-07-05 13:33:35 -0700

    Reorganize Views

--- app/src/main/java/me/rikinmarfatia/hydrohomie/MainActivity.kt
+++ app/src/main/java/me/rikinmarfatia/hydrohomie/MainActivity.kt
@@ -1,48 +1,21 @@
 package me.rikinmarfatia.hydrohomie
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.Providers
-import androidx.compose.getValue
-import androidx.compose.setValue
-import androidx.compose.state
-import androidx.ui.core.Alignment
 import androidx.ui.core.ContextAmbient
-import androidx.ui.core.Modifier
-import androidx.ui.core.clip
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Box
-import androidx.ui.foundation.ContentGravity
-import androidx.ui.foundation.Text
-import androidx.ui.graphics.Color
-import androidx.ui.layout.Column
-import androidx.ui.layout.Row
-import androidx.ui.layout.Spacer
-import androidx.ui.layout.fillMaxSize
-import androidx.ui.layout.fillMaxWidth
-import androidx.ui.layout.height
-import androidx.ui.layout.padding
-import androidx.ui.material.Button
-import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
 import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
 import com.github.zsoltk.compose.backpress.BackPressHandler
 import com.github.zsoltk.compose.router.Router
 import me.rikinmarfatia.hydrohomie.models.WaterKrate
-import me.rikinmarfatia.hydrohomie.models.WaterState
-import me.rikinmarfatia.hydrohomie.models.WaterTransition
 import me.rikinmarfatia.hydrohomie.theme.HydroHomieTheme
-import me.rikinmarfatia.hydrohomie.theme.hydroBlue
-import me.rikinmarfatia.hydrohomie.ui.DailyGoalDisplay
+import me.rikinmarfatia.hydrohomie.ui.DailyIntakeContainer
 import me.rikinmarfatia.hydrohomie.ui.HistoryContainer
 import me.rikinmarfatia.hydrohomie.ui.HistoryState
-import me.rikinmarfatia.hydrohomie.ui.ProfilePic
 import me.rikinmarfatia.hydrohomie.ui.Routing
-import me.rikinmarfatia.hydrohomie.ui.WaterGlass
 
 class MainActivity : AppCompatActivity() {
     private lateinit var waterStore: WaterKrate
@@ -60,10 +33,19 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     Router<Routing>("MainActivity", defaultRouting = Routing.Daily) { backStack ->
                         when (backStack.last()) {
-                            is Routing.Daily -> DailyTrackerContainer(context = this, store = waterStore) {
+                            is Routing.Daily -> DailyIntakeContainer(
+                                context = this,
+                                store = waterStore
+                            ) {
                                 backStack.push(Routing.History)
                             }
-                            is Routing.History -> HistoryContainer(state = HistoryState(days = listOf(waterStore.toWaterState())))
+                            is Routing.History -> HistoryContainer(
+                                state = HistoryState(
+                                    days = listOf(
+                                        waterStore.toWaterState()
+                                    )
+                                )
+                            )
                         }
                     }
                 }
@@ -78,91 +60,11 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
-@Composable
-fun DailyTrackerContainer(
-    context: Context,
-    store: WaterKrate,
-    onProfileClicked: () -> Unit = {}
-) {
-    // State that holds the information needed to render the screen
-    var waterState by state {
-        WaterState(
-            count = store.count,
-            goal = store.goal,
-            transition = WaterTransition(
-                current = store.count.toFloat() / store.goal
-            )
-        )
-    }
-
-    // Essentially a state reducer, updates the state based on this action
-    val addWaterAction: () -> Unit = {
-        val current = waterState.count.toFloat() / waterState.goal
-        val count = minOf(waterState.count + 1, waterState.goal)
-        val next = count.toFloat() / waterState.goal
-        val transition = WaterTransition(current, next)
-        waterState = waterState.copy(count = count, transition = transition)
-        store.count = count
-        context.sendBroadcast(Intent(context, HydroWidget::class.java).apply {
-            action = HydroWidget.ACTION_UPDATE_COUNT
-        })
-    }
-
-    val resetAction: () -> Unit = {
-        waterState = WaterState()
-        store.count = 0
-        context.sendBroadcast(Intent(context, HydroWidget::class.java).apply {
-            action = HydroWidget.ACTION_UPDATE_COUNT
-        })
-    }
-
-    // A very basic container, really easy to use to center everything
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        gravity = ContentGravity.Center,
-        backgroundColor = MaterialTheme.colors.background
-    ) {
-        // Container that layouts children vertically
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalGravity = Alignment.CenterHorizontally
-        ) {
-            ProfilePic(onProfileClicked)
-            Spacer(modifier = Modifier.height(16.dp))
-            WaterGlass(waterState)
-            // Container that lays out children horizontally
-            Row {
-                Button(
-                    onClick = addWaterAction,
-                    modifier = Modifier
-                        .padding(vertical = 16.dp, horizontal = 8.dp)
-                        .clip(MaterialTheme.shapes.large),
-                    backgroundColor = hydroBlue
-                ) {
-                    Text(text = "Add", color = MaterialTheme.colors.onSurface)
-                }
-                Button(
-                    onClick = resetAction,
-                    modifier = Modifier
-                        .padding(vertical = 16.dp, horizontal = 8.dp)
-                        .clip(MaterialTheme.shapes.large),
-                    backgroundColor = Color.Red
-                ) {
-                    Text(text = "Reset", color = MaterialTheme.colors.onSurface)
-                }
-            }
-            DailyGoalDisplay(waterState = waterState)
-        }
-    }
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun LightPreview() {
     HydroHomieTheme(darkTheme = false) {
-        DailyTrackerContainer(ContextAmbient.current, WaterKrate(ContextAmbient.current))
+        DailyIntakeContainer(ContextAmbient.current, WaterKrate(ContextAmbient.current))
     }
 }
 
@@ -170,6 +72,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     HydroHomieTheme(darkTheme = true) {
-        DailyTrackerContainer(ContextAmbient.current, WaterKrate(ContextAmbient.current))
+        DailyIntakeContainer(ContextAmbient.current, WaterKrate(ContextAmbient.current))
     }
 }
--- app/src/main/java/me/rikinmarfatia/hydrohomie/ui/DailyIntake.kt
+++ app/src/main/java/me/rikinmarfatia/hydrohomie/ui/DailyIntake.kt
@@ -1,27 +1,40 @@
 package me.rikinmarfatia.hydrohomie.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.animation.FastOutSlowInEasing
 import androidx.animation.FloatPropKey
 import androidx.animation.transitionDefinition
 import androidx.compose.Composable
+import androidx.compose.getValue
+import androidx.compose.setValue
+import androidx.compose.state
 import androidx.ui.animation.Transition
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.core.clip
 import androidx.ui.foundation.Box
+import androidx.ui.foundation.ContentGravity
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacer
 import androidx.ui.layout.Stack
 import androidx.ui.layout.fillMaxSize
 import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.height
+import androidx.ui.layout.padding
 import androidx.ui.layout.width
+import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
 import androidx.ui.text.style.TextAlign
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.Dp
 import androidx.ui.unit.dp
+import me.rikinmarfatia.hydrohomie.HydroWidget
+import me.rikinmarfatia.hydrohomie.models.WaterKrate
 import me.rikinmarfatia.hydrohomie.models.WaterState
 import me.rikinmarfatia.hydrohomie.models.WaterTransition
 import me.rikinmarfatia.hydrohomie.theme.hydroBlue
@@ -36,6 +49,84 @@ fun waterTransitionDefinition(waterState: WaterState) = transitionDefinition {
         waterHeightPercent using tween<Float> {
             duration = 300
             easing = FastOutSlowInEasing
+        }
+    }
+}
+
+@Composable
+fun DailyIntakeContainer(
+    context: Context,
+    store: WaterKrate,
+    onProfileClicked: () -> Unit = {}
+) {
+    // State that holds the information needed to render the screen
+    var waterState by state {
+        WaterState(
+            count = store.count,
+            goal = store.goal,
+            transition = WaterTransition(
+                current = store.count.toFloat() / store.goal
+            )
+        )
+    }
+
+    // Essentially a state reducer, updates the state based on this action
+    val addWaterAction: () -> Unit = {
+        val current = waterState.count.toFloat() / waterState.goal
+        val count = minOf(waterState.count + 1, waterState.goal)
+        val next = count.toFloat() / waterState.goal
+        val transition = WaterTransition(current, next)
+        waterState = waterState.copy(count = count, transition = transition)
+        store.count = count
+        context.sendBroadcast(Intent(context, HydroWidget::class.java).apply {
+            action = HydroWidget.ACTION_UPDATE_COUNT
+        })
+    }
+
+    val resetAction: () -> Unit = {
+        waterState = WaterState()
+        store.count = 0
+        context.sendBroadcast(Intent(context, HydroWidget::class.java).apply {
+            action = HydroWidget.ACTION_UPDATE_COUNT
+        })
+    }
+
+    // A very basic container, really easy to use to center everything
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        gravity = ContentGravity.Center,
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        // Container that layouts children vertically
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalGravity = Alignment.CenterHorizontally
+        ) {
+            ProfilePic(onProfileClicked)
+            Spacer(modifier = Modifier.height(16.dp))
+            WaterGlass(waterState)
+            // Container that lays out children horizontally
+            Row {
+                Button(
+                    onClick = addWaterAction,
+                    modifier = Modifier
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
+                        .clip(MaterialTheme.shapes.large),
+                    backgroundColor = hydroBlue
+                ) {
+                    Text(text = "Add", color = MaterialTheme.colors.onSurface)
+                }
+                Button(
+                    onClick = resetAction,
+                    modifier = Modifier
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
+                        .clip(MaterialTheme.shapes.large),
+                    backgroundColor = Color.Red
+                ) {
+                    Text(text = "Reset", color = MaterialTheme.colors.onSurface)
+                }
+            }
+            DailyGoalDisplay(waterState = waterState)
         }
     }
 }
@@ -99,7 +190,7 @@ fun DailyGoalDisplay(waterState: WaterState) {
     )
 }
 
-@Preview(showDecoration = true)
+@Preview
 @Composable
 fun WaterBoxPreview() {
     WaterGlass(
@@ -114,7 +205,7 @@ fun WaterBoxPreview() {
     )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DailyGoalDisplayPreview() {
     DailyGoalDisplay(waterState = WaterState(count = 4))
